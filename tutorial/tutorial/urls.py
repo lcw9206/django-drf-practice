2473d3dcb86435282096612ce1fe670dc7bc3586
Lee <lcw9206@gmail.com>
2018-06-12 01:37:29 +0900

    Adding login to Browsable API

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('snippets.urls')),
+    url(r'^api-auth/', include('rest_framework.urls')),
 ]
